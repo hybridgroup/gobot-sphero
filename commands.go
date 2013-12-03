3a0a17fa03e7e0f6adc46b44eb4e40d9fb6e0e52
Adrian Zankich <adrian@hybridgroup.com>
2013-12-02 19:04:43 -0800

    Add more commands

--- commands.go
+++ commands.go
@@ -6,3 +6,13 @@ func (sd *SpheroDriver) SetRGBApi(params map[string]interface{}) {
 	b := uint8(params["b"].(float64))
 	sd.SetRGB(r, g, b)
 }
+
+func (sd *SpheroDriver) RollApi(params map[string]interface{}) {
+	speed := uint8(params["speed"].(float64))
+	heading := uint16(params["heading"].(float64))
+	sd.Roll(speed, heading)
+}
+
+func (sd *SpheroDriver) StopApi() {
+	sd.Stop()
+}
--- sphero_driver.go
+++ sphero_driver.go
@@ -22,7 +22,11 @@ func NewSphero(sa *SpheroAdaptor) *SpheroDriver {
 	s := new(SpheroDriver)
 	s.Events = make(map[string]chan interface{})
 	s.SpheroAdaptor = sa
-	s.Commands = []string{"SetRGBApi"}
+	s.Commands = []string{
+		"SetRGBApi",
+		"RollApi",
+		"StopApi",
+	}
 	return s
 }
 
